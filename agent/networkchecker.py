ece0a60da3723a27c4c9cc4ea3656fc936674d4f
TechBK <quangbinh.nguyentrong@gmail.com>
2016-04-21 02:59:11 +0700

    version v0.0.2

--- agent/networkchecker.py
+++ agent/networkchecker.py
@@ -2,26 +2,31 @@ import asyncio
 from abc import ABCMeta, abstractmethod
 import importlib
 import logging
-from godeye_agent import config
+from agent import config
 
 
 logger = logging.getLogger(__name__)
 
 
 class AbstractNetworkChecker(metaclass=ABCMeta):
-    def __init__(self, client, loop):
+    def __init__(self, client, loop, queue):
         self._plugins = []
         self._client = client
         self._loop = loop
+        self._queue = queue
+
+        # cai nay de duoi cung
         self._load_plugins()
+
 
 
     def _load_plugins(self):
         for check in config.enable_plugin:
             name = check['name']
             path = check['path']
+            kwarg = check.get('kwarg', {})
             try:
-                plugin = self._load_plugin(name, path)
+                plugin = self._load_plugin(name, path, **kwarg)
             except Exception as e:
                 logger.info('Error: {!r}'.format(e))
                 plugin = None
@@ -31,19 +36,19 @@ class AbstractNetworkChecker(metaclass=ABCMeta):
                 self._plugins.append(plugin)
 
 
-    def _load_plugin(self, name, path):
+    def _load_plugin(self, name, path, **kwarg):
         plugin = importlib.import_module(path, __name__)
-        return getattr(plugin, name)(self._loop)
+        return getattr(plugin, name)(self._loop, self._queue, **kwarg)
 
     @abstractmethod
     def __call__(self, *args, **kwargs):
         raise NotImplementedError('AbtractNetworkChecker cant call')
 
     @asyncio.coroutine
-    def real_call(self, node_target):
+    def real_call(self, dnode):
         if self._plugins:
-            cors = [plugin(self._client, node_target) for plugin in self._plugins]
-            r = asyncio.wait(cors, loop=self._loop)
+            cors = [plugin(self._client, dnode) for plugin in self._plugins]
+            r = yield from asyncio.wait(cors, loop=self._loop)
             return r
 
         raise Exception('No Plugin')
@@ -53,8 +58,8 @@ class AbstractNetworkChecker(metaclass=ABCMeta):
 class NetworkChecker(AbstractNetworkChecker):
     def __call__(self, get_node):
         logger.info('Network checker start running')
-        node_target = get_node()
-        future = asyncio.run_coroutine_threadsafe(self.real_call(node_target), self._loop)
+        dnode = get_node()
+        future = asyncio.run_coroutine_threadsafe(self.real_call(dnode), self._loop)
         try:
             result = future.result(config.check_timeout)  # Wait for the result with a timeout
         except asyncio.TimeoutError:
@@ -63,6 +68,19 @@ class NetworkChecker(AbstractNetworkChecker):
         except Exception as exc:
             logger.info('The coroutine raised an exception: {!r}'.format(exc))
         else:
-            logger.info('The coroutine returned: {!r}'.format(result))
+            logger.info('The Checks returned: {!r}'.format(result))
             logger.info('Network checker end!!!')
 
+        # logger.info('Network checker start running')
+        # node_target = get_node()
+        # try:
+        #     task = asyncio.async(asyncio.wait_for(self.real_call(node_target), self._loop), self._loop)
+        #     # result = future.result(config.check_timeout)  # Wait for the result with a timeout
+        # except asyncio.TimeoutError:
+        #     logger.info('The coroutine took too long, cancelling the task...')
+        #     task.cancel()
+        # except Exception as exc:
+        #     logger.info('The coroutine raised an exception: {!r}'.format(exc))
+        # else:
+        #     logger.info('The coroutine returned: {!r}'.format(result))
+        #     logger.info('Network checker end!!!')
--- docs/Agent.md
+++ docs/Agent.md
@@ -5,3 +5,10 @@
     + Ping plugin
     + hardcode config
     + phần main dùng scheduler
+    
+
+- v0.0.2:
+> mục tiêu thêm tính năng để hoàn thiện phần Ping plugin:
+    + add 1 coroutine get_result vào plugin
+    + viết Send Result component
+    + doi ten file agent.py -> g_agent.py de tranh trung ten voi thu muc agent
