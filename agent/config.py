373c45f9eaa4b6ce000afd5eabb464ac914db4d3
TechBK <quangbinh.nguyentrong@gmail.com>
2016-04-25 15:12:34 +0700

    Revert "Merge pull request #3 from PiScale/kiennt"
    This reverts commit 5f50a08e2afe853ba462bc153f89e6040f7d8ef1, reversing
    changes made to acb44d779d159b1e8dc9b4c394a771dd1f172ed8.

--- agent/config.py
+++ agent/config.py
@@ -1,8 +1,8 @@
-rpc_server_url = None  # địa chỉ rpc_server
-db_url = None  # địa chỉ của MetricDB
+rpc_server_url = None # địa chỉ rpc_server
+db_url = None #địa chỉ của MetricDB
 check_timeout = 5
-upload_timeout = None  # timeout of sending result to DB
-check_interval = 10  # option which APScheduler need
+upload_timeout = None # timeout of sending result to DB
+check_interval = 10 # option which APScheduler need
 
 
 enable_plugin = [
@@ -11,11 +11,3 @@ enable_plugin = [
         'path': 'check_plugins.ping'
     }
 ]
-
-# InfluxDB connect configuration
-INFLUXDB_HOST = '127.0.0.1'  # InfluxDB Server
-INFLUXDB_PORT = 8086
-INFLUXDB_USERNAME = 'root'
-INFLUXDB_PASSWORD = 'secret_password'
-INFLUXDB_DBNAME = 'database_name'
-INFLUXDB_DBPASSWORD = 'database_secret_password'
--- agent/sendresult.py
+++ agent/sendresult.py
@@ -1,16 +1,14 @@
 import asyncio
+import aiohttp
 import logging
-from influxdb import InfluxDBClient
 from agent import config
 
 logger = logging.getLogger(__name__)
 
 
 class SendResult(object):
-
     def __init__(self, queue):
         self._queue = queue
-        self.client = Connector.get_client()
 
     @asyncio.coroutine
     def __call__(self):
@@ -21,32 +19,12 @@ class SendResult(object):
         """
 
         # Neu Kien ko dung client cua aiohttp thi ko can dung dong nay.
-        # with aiohttp.ClientSession() as client:
-        while True:
-            cor_result = yield from self._queue.get()
-            result = yield from cor_result
-            logger.info(result)
-            if self.client is not None:
-                self.client.write_points(result)
-            else:
-                raise Exception(
-                    "We get some problems with database connection")
+        with aiohttp.ClientSession() as client:
+            while True:
+                cor_result = yield from self._queue.get()
+                result = yield from cor_result
+                logger.info(result)
+                # CHo nay Kien viet ham gui data....
 
 
-class Connector(object):
-
-    def __init__(self):
-        self.host = config.INFLUXDB_HOST
-        self.port = config.INFLUXDB_PORT
-        self.username = config.INFLUXDB_USERNAME
-        self.password = config.INFLUXDB_PASSWORD
-        self.database = config.INFLUXDB_DBPASSW
-
-    def get_client(self):
-        client = None
-        try:
-            client = InfluxDBClient(self.host, self.port, self.username,
-                                    self.password, self.database)
-        except Exception as err:
-            print("Connect to InfluxDB failed, due to: ", err)
-        return client
+                # client.post(config.db_url, data=result)
